8e0c8c55b5404277b83dad92ae8a35530f7c7899
zahid7880 <hussain.zahid7880@gmail.com>
2021-11-24 17:26:02 +0500

    updated

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const form = document.querySelector('#form');
 const input = document.querySelector('#input');
-const todosUL = document.querySelector('#todos');
+const todosUl = document.querySelector('#todoUl');
 
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', function (e) {
     e.preventDefault()
 
     addTodo()
@@ -11,48 +11,63 @@ form.addEventListener('submit', (e) => {
 function addTodo(todo) {
     let todoText = input.value
 
-    if(todo){
-        todoText = todo.text;
+    if(todo) {
+        todoText = todo.text
     }
-    
+
     if(todoText) {
-        const todoElem = document.createElement('li');
+        const todoElem = document.createElement('li')
 
-        if(todo && todo.completed) {
-            todoElem.classList.add('completed')
-        }
+        todoElem.innerText = todoText;
+        todosUl.appendChild(todoElem)
+        console.log(todoElem)
 
-        todoElem.addEventListener('click', () => {
-            todoElem.classList.toggle('completed')
-            updateLis()
-        })
-
-        todoElem.addEventListener('contextmenu', (e) => {
-            e.preventDefault()
-            todoElem.remove()
-        })
-        
-        todoElem.innerText = todoText
-        todosUL.appendChild(todoElem)
-
-        input.value = ''
-        
-        updateLis()
+        input.value = '';
     }
-
 }
 
-function updateLis() {
-    const todosElem = document.querySelectorAll('li');
 
-    const todos = [];
+// function addTodo(todo) {
+//     
+    
+//     if(todoText) {
+//         const todoElem = document.createElement('li');
 
-    todosElem.forEach(todoElem => {
-        todos.push({
-            test: todoElem.innerText,
-            completed: todoElem.classList.contains('completed')
-        })
-    })
+//         if(todo && todo.completed) {
+//             todoElem.classList.add('completed')
+//         }
 
-    console.log(todos)
-}
+//         todoElem.addEventListener('click', () => {
+//             todoElem.classList.toggle('completed')
+//             updateLis()
+//         })
+
+//         todoElem.addEventListener('contextmenu', (e) => {
+//             e.preventDefault()
+//             todoElem.remove()
+//         })
+        
+//         todoElem.innerText = todoText
+//         todosUL.appendChild(todoElem)
+
+//         input.value = ''
+        
+//         updateLis()
+//     }
+
+// }
+
+// function updateLis() {
+//     const todosElem = document.querySelectorAll('li');
+
+//     const todos = [];
+
+//     todosElem.forEach(todoElem => {
+//         todos.push({
+//             test: todoElem.innerText,
+//             completed: todoElem.classList.contains('completed')
+//         })
+//     })
+
+//     console.log(todos)
+// }
